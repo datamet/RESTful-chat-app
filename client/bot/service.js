2f37e7e9881a7eb5ae77b5b97c811d97222a20f4
Erik Storås Sommer <eriksommer99@gmail.com>
2021-04-08 23:28:21 +0200

    Exports method for random bot and password

--- client/bot/service.js
+++ client/bot/service.js
@@ -1,5 +1,11 @@
-const botnames = ["Olav", "Iselin", "Magnus", "Mari", "Oliver", "Tobias"]
+const botnames = ["Olav", "Iselin", "Magnus", "Mari", "Oliver", "Tobias"];
+const password = ["Olav1234", "Iselin1234", "Magnus1234", "Mari1234", "Oliver1234", "Tobias1234"];
 
 export const randomebot = () => {
-    return botnames[Math.floor(Math.random() * botnames.length)];
+    const i = Math.floor(Math.random() * botnames.length)
+    const bot = {
+        username: botnames[i],
+        password: password[i]
+    }
+    return bot;
 }
