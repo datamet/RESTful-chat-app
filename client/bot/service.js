e8db50f921d3cae0e6c19cd8e9578e3143a2847d
Erik Storås Sommer <eriksommer99@gmail.com>
2021-04-11 20:55:18 +0200

    Exports default method

--- client/bot/service.js
+++ client/bot/service.js
@@ -1,7 +1,7 @@
 const botnames = ["Olav", "Iselin", "Magnus", "Mari", "Oliver", "Tobias"];
 const password = ["Olav1234", "Iselin1234", "Magnus1234", "Mari1234", "Oliver1234", "Tobias1234"];
 
-export const randomebot = () => {
+export default () => {
     const i = Math.floor(Math.random() * botnames.length)
     return {
         username: botnames[i],
