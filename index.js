6c060adcd9ebbf85db402f54f8036bd64a43cebd
Mats Sommervold <mats@sommervold.net>
2021-04-14 15:19:08 +0200

    Commenting server file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  const { server } = require('./server/server')
  const config = require('./server/lib/config')
  
- // Starting http rest server
+ // Starting server
  server.listen(config.port, () => {
      console.log(`[server] server started on ${config.port}`)
      console.log(`[mode] Server's started in ${config.mode} mode`)
--- server/server.js
+++ server/server.js
@@ -15,25 +15,25 @@ const authenticator = require('./middleware/authenticator')
 const apiRouter = require('./routes/api/api.routes')
 const errorHandler = require('./middleware/errorHandler')
 
-// Creating web socket server
+// Creating server
 const server = http.createServer()
 
-// Creating rest app and passing it to server
+// Creating express app and passing it to server
 const app = express()
 server.on('request', app)
 
-// Create websoket app and passing it server
+// Create websoket app and passing server to it
 const ws = createWS(server)
 
-// Path to frontend application
+// Path to static frontend
 const public_path = path.join(__dirname, '../app/public')
 
 // Using app level middleware
-app.use(cors())
-app.use(bodyParser)
-app.use(authenticator)
-app.use(apiRouter)
-app.use(express.static(public_path))
-app.use(errorHandler)
+app.use(cors())                         // Enabeling cross origin requests
+app.use(bodyParser)                     // Parsing body to json
+app.use(authenticator)                  // Validates that the user has access to resource
+app.use(apiRouter)                      // Passing request to router
+app.use(express.static(public_path))    // Using static router to deliver frontend application
+app.use(errorHandler)                   // Handeling erros and sending them back to the client
 
 module.exports = { server }
