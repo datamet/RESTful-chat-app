3b026de286b59e621749896ace592f22d29c47dc
Mats Sommervold <mats@sommervold.net>
2021-04-14 13:15:31 +0200

    integrating new bots

--- client/bot/mind.js
+++ client/bot/mind.js
@@ -9,6 +9,8 @@ subjects.set('programmer', 'a person that fixed a problem you dont know you have
 subjects.set('oslo', 'the capital of Norway')
 
 const res = {
+    starter: ['Hey, people!', 'Hi there, I am a bot', 'Hey lads', 'Good [day]', 'Hi, [question]'],
+    ender: ['I have to go', 'cya, I have to go', 'Leaving. Talk to you later', 'Bye all'],
     question: ['How are you?', 'Do you like [food]?', 'If you could live anywhere, where would it be?',
     'Are you [mood]?'],
     statement: ['The world is [mood]!', '[food] is [mood]', 'I [attitude] [cloathing]'],
@@ -17,7 +19,8 @@ const res = {
     mood: ['good', 'great', 'fine', 'ok', 'happy', 'sad', 'awesome'],
     food: ['pizza', 'meatballs', 'chocolate','chips', 'cookies', 'taco', 'burito', 'pasta', 'rice', 'salad'],
     like: ['I [attitude] [subject]'],
-    what: ['[subject] is [explanation]'],
+    haha: [':D', ':)', 'xD', 'Time goes faster when having fun!', 'The world needs more fun stuff', 'ahhahaha'],
+    what: ['[subject] is [explanation]', 'I dont know a lot about [subject], but i know it is [explanation]'],
     unknown: ["I dont know what that is yet. You could try telling me by saying thing = explanation"],
     joke: ['Hardware. The part of a computer you can kick', 'What is the object oriented way to become wealty? Inheritance',
     'What do you call programmers from Norway? Nerdic', 'What is a programmers hangout place? Foo bar'],
@@ -36,7 +39,8 @@ const res = {
     understand: ["I didn't quite catch that", "I don't understand. Let's talk about something else. [statement]", 
     "That sentence was a bit too advanced for me"],
     saved: ['Now I know what [subject] is!', 'Thank you for explaining what [subject] is'],
-    inactive: ['Are you still here?', 'Anyone here?', 'Hellooooooooooo..', 'Someone here?', 'I guess everyone left.']
+    inactive: ['Are you still here?', 'Anyone here?', 'Hellooooooooooo..', 'Someone here?', 'I guess everyone left.'],
+    else: ['On to something else. [statement]', 'On to something else. [question]', "Let's talk about sometihng else!"]
 }
 
 // Checks if mastermind has managed to pinpoint the subject
@@ -48,6 +52,7 @@ const subjectExsists = (original) => {
 triggers.set('=', () => subjectExsists(res.saved))
 triggers.set(' do you like ', () => subjectExsists(res.like))
 triggers.set(' what is ', () => subjectExsists(res.what))
+triggers.set(' who is ', () => subjectExsists(res.what))
 triggers.set(' explain ', () => subjectExsists(res.what))
 triggers.set(' tell us about ', () => subjectExsists(res.what))
 triggers.set(' tell me about ', () => subjectExsists(res.what))
@@ -62,6 +67,7 @@ triggers.set(' whats up ', () => res.hi)
 triggers.set(' how are you ', () => res.how)
 
 triggers.set(' bot ', () => res.bot)
+triggers.set(' who are you ', () => res.bot)
 triggers.set(' are you real ', () => res.bot)
 triggers.set(' you are real ', () => res.bot)
 triggers.set(' you are real ', () => res.bot)
@@ -75,18 +81,32 @@ triggers.set(' ok ', () => res.ok)
 triggers.set(' okey ', () => res.ok)
 triggers.set(' nope ', () => res.no)
 triggers.set(' no ', () => res.no)
+triggers.set(' nah ', () => res.no)
+triggers.set(' nix ', () => res.no)
 triggers.set(' sorry ', () => res.sorry)
 triggers.set(' is bad ', () => res.bad)
-triggers.set(' agree ', () => res.like)
-triggers.set(' cool ', () => res.like)
-triggers.set(' nice ', () => res.like)
-triggers.set(' right on ', () => res.like)
-triggers.set(' to bad ', () => res.like)
-triggers.set(' no worries ', () => res.like)
+triggers.set(' agree ', () => res.else)
+triggers.set(' cool ', () => res.else)
+triggers.set(' nice ', () => res.else)
+triggers.set(' right on ', () => res.else)
+triggers.set(' to bad ', () => res.else)
+triggers.set(' no worries ', () => res.else)
 triggers.set(' thank you ', () => res.thanks)
 triggers.set(' thanks ', () => res.thanks)
 triggers.set(' ty ', () => res.thanks)
 triggers.set(' thankyou ', () => res.thanks)
+
+triggers.set(' i am fine ', () => res.cool)
+triggers.set(' i am good ', () => res.cool)
+triggers.set(' i am great ', () => res.cool)
+triggers.set(' i am ok ', () => res.ok)
+triggers.set(' i am ill ', () => res.no)
+triggers.set(' i am happy ', () => res.cool)
+triggers.set(' i am down ', () => res.no)
+triggers.set(' i am depressed ', () => res.no)
+triggers.set(' i am overwhelmed ', () => res.no)
+triggers.set(' i am stressed ', () => res.no)
+triggers.set(' i am not good ', () => res.no)
 
 triggers.set(' tell me a joke ', () => res.joke)
 triggers.set(' tell us a joke ', () => res.joke)
@@ -103,17 +123,23 @@ triggers.set(' what year ', () => res.date)
 triggers.set(' what month ', () => res.date)
 triggers.set(' what day ', () => res.date)
 
-// fix these
+triggers.set(' lol ', () => res.haha)
+triggers.set(' haha ', () => res.haha)
+triggers.set(' hahaha ', () => res.haha)
+triggers.set(' hah ', () => res.haha)
+triggers.set(' hahahah ', () => res.haha)
+triggers.set(' ahahha ', () => res.haha)
+triggers.set(' ahah ', () => res.haha)
+triggers.set(' ahahhaha ', () => res.haha)
+
 triggers.set(' goodbye ', () => res.bye)
 triggers.set(' bye ', () => res.bye)
 triggers.set(' be back ', () => res.bye)
 triggers.set(' i am leaving ', () => res.bye)
 triggers.set(' good bye ', () => res.bye)
 triggers.set(' see you ', () => res.bye)
-triggers.set(' please leave ', () => res.date)
-triggers.set(' go away ', () => res.date)
-triggers.set(' turn off ', () => res.date)
-triggers.set(' kick bot ', () => res.date)
+triggers.set(' goodnight ', () => res.bye)
+triggers.set(' good night ', () => res.bye)
 
 // Pick random element of array
 const rand = arr => arr[Math.floor(Math.random() * arr.length)];
@@ -144,47 +170,56 @@ const createRespons = reply =>{
 // Add message context before creating message
 const addContext = ({ username }, sender, message) => {
     res.username = [username]
-    res.sender = [sender]
+    if (sender) res.sender = [sender]
     const date = new Date()
     const hours = date.getHours()
     const isDayTime = hours > 6 && hours < 20
     res.time = [isDayTime ? 'day' : 'night', `${hours} o'clock`]
+    res.day = [isDayTime ? 'day' : 'night']
     const fullDate = `${date.getDate()}.${date.getMonth()}.${date.getFullYear()}`
     res.date = [`Today is ${fullDate}`, `The date is ${fullDate}`]
     
     // Check for subject matches
-    if (message.includes('=')) {
-        let splitMessage = message.split('=')
-        if (splitMessage.length === 2) {
-            res.subject = [splitMessage[0].trim()]
-            res.explanation = [splitMessage[1].slice(1)]
-            subjects.set(splitMessage[0].trim(), splitMessage[1].slice(1))
-        }
-        else {
-            res.subject = null
-            res.explanation = null
-        }
-    }
-    else {
-        for (const [ subject, explanation ] of subjects) {
-            if (message.includes(subject)) {
-                res.subject = [subject]
-                res.explanation = [explanation]
-                break
-            } else {
+    if (message) {
+        if (message.includes('=')) {
+            let splitMessage = message.split('=')
+            if (splitMessage.length === 2) {
+                res.subject = [splitMessage[0].trim()]
+                res.explanation = [splitMessage[1].slice(1)]
+                subjects.set(splitMessage[0].trim(), splitMessage[1].slice(1))
+            }
+            else {
                 res.subject = null
                 res.explanation = null
             }
         }
+        else {
+            for (const [ subject, explanation ] of subjects) {
+                if (message.includes(subject)) {
+                    res.subject = [subject]
+                    res.explanation = [explanation]
+                    break
+                } else {
+                    res.subject = null
+                    res.explanation = null
+                }
+            }
+        }
+    }
+    else {
+        res.subject = null
+        res.explanation = null
     }
 }
 
 export default info => (msg, options) => {
     // check that message is not null
     let response = '', processed = ''
-    if (!msg) createRespons(res.inactive)
+    addContext(info, msg ? msg.sender : null, msg ? msg.message : null)
+    if (options ? options.starter : false) return createRespons(res.starter)
+    if (options ? options.ender : false) return createRespons(res.ender)
+    if (!msg) return createRespons(res.inactive)
     const { sender, message } = msg
-    addContext(info, sender, message)
     if (message) {
         // Process input to be easier to match
         processed = message.toLowerCase()
